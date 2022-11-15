c2f710e763e17270365108051212114aacdaf176
Muhammad Bilal Jamil <70377341+mbjamil92@users.noreply.github.com>
2022-11-14 20:52:51 -0500

    updating lambda code and req file

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,62 @@
 import pandas as pd
+import boto3
+import os
+from dotenv import load_dotenv
+import logging
+import sys
+import time
+import datetime as dt
+import io
+
+####### LOADING ENVIRONMENT VARIABLES #######
+load_dotenv()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+BUCKET = os.getenv('BUCKET')
+BUCKET_PREFIX = os.getenv('BUCKET_PREFIX')
+
+# Credentials to database connection
+hostname= os.getenv('HOSTNAME')
+dbname= os.getenv('DATABASE')
+uname= os.getenv('USERNAME')
+pwd= os.getenv('PASSWORD')
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.2')
+    try:
+        logger.info("TEST")
+        logger.info(BUCKET)
+
+        s3 = boto3.resource('s3')
+
+        # assigning the bucket:
+        my_bucket = s3.Bucket(BUCKET)
+
+        data_list = []
+
+        for my_bucket_object in my_bucket.objects.filter(Prefix=BUCKET_PREFIX):
+            if my_bucket_object.key.endswith(".csv"):
+                key=my_bucket_object.key
+                body=my_bucket_object.get()['Body'].read()
+                temp_data = pd.read_csv(io.BytesIO(body))
+                data_list.append(temp_data)
+            df = pd.concat(dat_list)
+            small_data = df.head(20)
+
+            # Create SQLAlchemy engine to connect to MySQL Database
+            engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+				.format(host=hostname, db=dbname, user=uname, pw=pwd))
+
+            # Truncate the table everytime before an ETL:
+            engine.execute("TRUNCATE TABLE irs990")
+
+            # Convert dataframe to sql table                                   
+            small_data.to_sql('irs990', engine, if_exists='append',index=False)
+
+            # checking if data was successfully written:
+            engine.execute("SELECT * FROM irs990 limit 10").fetchall()
+
+            # closing engine connection by deleting it:
+            del engine
+        except Exception as e:
+            logging.error(e)
